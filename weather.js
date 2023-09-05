d392e41cb86e179bc9f12f7d518d5a9a6244dedc
Amansingh293 <amansinghdl9c@gmail.com>
2023-09-05 16:46:04 +0530

    15th commit

--- weather.js
+++ weather.js
@@ -1,119 +1,123 @@
+const btnElement = document.querySelector(".btn");
 
-const btnElement = document.querySelector('.btn');
-
-const inputElement = document.querySelector('.input');
+const inputElement = document.querySelector(".input");
 // console.log(btnElement);
-const displayTitle = document.querySelector('h2');
+const displayTitle = document.querySelector("h2");
 
-const apiKey = '00f06710edcf484f936125141230309';
+const apiKey = "00f06710edcf484f936125141230309";
 
-const tempELementC = document.querySelector('.tempElementC');
+const tempELementC = document.querySelector(".tempElementC");
 
-const tempELementF = document.querySelector('.tempElementF');
+const tempELementF = document.querySelector(".tempElementF");
 
-const currentCity = document.querySelector('.city');
+const currentCity = document.querySelector(".city");
 
-const currentCountry = document.querySelector('.country');
+const currentCountry = document.querySelector(".country");
 
-const currentTime = document.querySelector('.time');
+const currentTime = document.querySelector(".time");
 
-const currentCondition = document.querySelector('.condition');
+const currentCondition = document.querySelector(".condition");
 
-const image = document.querySelector('.imgLink');
+const image = document.querySelector(".imgLink");
 
-const currentDate = document.querySelector('.date');
+const currentDate = document.querySelector(".date");
 
-btnElement.addEventListener('click' , function(e){
+btnElement.addEventListener("click", function (e) {
+  initiator(e);
+});
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
     initiator(e);
+  }
+  // console.log(e.key);
 });
 
-document.addEventListener('keypress' , function(e){
+function initiator(obj) {
+  btnElement.classList.add("btnEffects");
 
-    if( e.key === 'Enter'){
-        initiator(e);
-    }
-    // console.log(e.key);
-});
+  setTimeout(() => {
+    btnElement.classList.remove("btnEffects");
+  }, 200);
 
-function initiator(obj){
+  if (inputElement.value === "") {
+    alert("Please Enter a Valid Location");
+    return;
+  }
 
-    btnElement.classList.add('btnEffects');
+  const city = inputElement.value;
 
-    setTimeout(() => {
-        btnElement.classList.remove('btnEffects'); 
-     }, 200);
+  fetchNUpdateUI(city);
 
-    if( inputElement.value === ''){
-        alert('Please Enter a Valid Location');
-        return;
-    }
-
-    const city = inputElement.value;
-
-    fetchNUpdateUI(city);
-
-    inputElement.value = '';
-
+  inputElement.value = "";
 }
-async function fetchNUpdateUI(cityName){
+async function fetchNUpdateUI(cityName) {
+  try {
+    let url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`;
+    let response = await fetch(url);
+    let json = await response.json();
+    // console.log(json);
 
-    try{
-        let url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`;
-        let response = await fetch(url);
-        let json = await response.json();
-        // console.log(json);
-
-        if(response.status === 400){
-            alert(`${json.error.message}`);
-            return;
-        }
-        const tempInC = json.current.temp_c;
-
-        const tempInF = json.current.temp_f;
-        
-        const cityVal = json.location.name;
-
-        const currCountry = json.location.country;
-
-        const time = json.location.localtime;
-
-        const currCondition = json.current.condition.text;
-
-        const imageLink = json.current.condition.icon;
-
-        uiChange(tempInF , tempInC , cityVal , currCountry , time , currCondition , imageLink);
-        
+    if (response.status === 400) {
+      alert(`${json.error.message}`);
+      return;
     }
-    catch(err){
-        // console.log(err);
-    }
+    const tempInC = json.current.temp_c;
+
+    const tempInF = json.current.temp_f;
+
+    const cityVal = json.location.name;
+
+    const currCountry = json.location.country;
+
+    const time = json.location.localtime;
+
+    const currCondition = json.current.condition.text;
+
+    const imageLink = json.current.condition.icon;
+
+    uiChange(
+      tempInF,
+      tempInC,
+      cityVal,
+      currCountry,
+      time,
+      currCondition,
+      imageLink
+    );
+  } catch (err) {
+    // console.log(err);
+  }
 }
 
+function uiChange(
+  tempInF,
+  tempInC,
+  cityVal,
+  currCountry,
+  time,
+  currCondition,
+  imageLink
+) {
+  const date = time.substring(0, 10);
 
-function uiChange(tempInF , tempInC , cityVal , currCountry , time , currCondition , imageLink){
+  // date.reverse();
 
-    const date = time.substring(0,10);
+  const timePart = time.substring(11, 16);
 
-    // date.reverse();
+  tempELementC.innerText = `${tempInC} °C`;
 
-    const timePart = time.substring(11,16);
+  tempELementF.innerText = `${tempInF} °F`;
 
-    tempELementC.innerText = `${tempInC} °C`;
+  currentCity.innerText = cityVal;
 
-    tempELementF.innerText = `${tempInF} °F`;
+  currentTime.innerText = timePart;
 
-    currentCity.innerText = cityVal;
+  currentDate.innerText = date;
 
-    currentTime.innerText = timePart;
+  currentCountry.innerText = currCountry;
 
-    currentDate.innerText = date;
+  image.setAttribute("src", imageLink);
 
-    currentCountry.innerText = currCountry;
-
-    image.setAttribute('src' , imageLink);
-
-    currentCondition.innerText = currCondition;
+  currentCondition.innerText = currCondition;
 }
-
-
-
